4ec3bbc2ea426c26a363dbc4a9efc809f2c2dd39
KatynD8 <decker.thomas2911@gmail.com>
2025-08-31 19:19:49 +0200

    refound hero-banner and fonts

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,22 @@ document.getElementById("lang-switch").addEventListener("change", function () {
     }
   }
 });
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const heroContent = document.querySelector(".hero-content");
+  if (heroContent) {
+    heroContent.classList.add("visible");
+  }
+
+  const heroElements = document.querySelectorAll(
+    ".hero_content_left h1, .hero_content_left a, .hero_texts h4, .hero_texts img"
+  );
+
+  heroElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("visible");
+    }, 200 + index * 200);
+  });
+});
