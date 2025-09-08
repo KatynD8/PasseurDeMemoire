b2d0d9291200e0962f25d04060e3735b72fad607
KatynD8 <130679149+KatynD8@users.noreply.github.com>
2025-09-08 13:35:28 +0200

    final design

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   initLangSwitcher();
   initHeroAnimations();
 });
+const wrapper = document.querySelector(".testimonials-wrapper");
+document.querySelector(".scroll-left").addEventListener("click", () => {
+  wrapper.scrollBy({ left: -400, behavior: "smooth" });
+});
+document.querySelector(".scroll-right").addEventListener("click", () => {
+  wrapper.scrollBy({ left: 400, behavior: "smooth" });
+});
